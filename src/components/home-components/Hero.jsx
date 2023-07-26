c612d820db0a676b8ecbd66e1fdf3b1d58b0dfc0
Umar Jihad <umarjihad141004@gmail.com>
2023-07-26 19:07:50 +0800

    add hero section

---
+++ src/components/home-components/Hero.jsx
@@ -0,0 +1,11 @@
+import "../../style/Hero.scss"
+
+export default function Hero(){
+    return (
+        <section className="hero">
+            <div className="tagline">
+                Unleash Your <span>Inner Warrior</span>
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Hero from "../components/home-components/Hero";
 
 export default function Home(){
     return (
         <>
         <Navbar />
+        <Hero />
         <Footer />
         </>
     )
