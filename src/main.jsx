a23dbb9e16010ce7b25f318812d1f143869a4996
Umar Jihad <umarjihad141004@gmail.com>
2023-07-26 09:24:24 +0800

    add vite, react, anda sass into the golden fist project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+
+function App(){
+    return <h1>Hai</h1>
+}
+
+ReactDOM.createRoot(document.body).render(<App />)
