fab3cdbf9109062edd7d24f9e0b497fad908d862
jihadable <umarjihad141004@gmail.com>
2023-10-16 21:35:00 +0700

    add newsletter subscribe form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { IconMail } from "@tabler/icons-react"
 import logo from "../assets/golden-fist-logo.png"
 import "../style/Footer.scss"
 import { IconBrandInstagram, IconBrandTiktok, IconBrandYoutube } from "@tabler/icons-react"
@@ -14,32 +15,44 @@ export default function Footer(){
 
     return (
         <footer className="footer">
-            <div className="footer-left">
-                <div className="logo">
-                    <img src={logo} alt="Logo" />
+            <div className="footer-top">
+                <div className="text">Subscribe to our newsletter now and be the first to know about Golden Fist's latest news!</div>
+                <div className="input-btn">
+                    <div className="input">
+                        <IconMail stroke={1.5} />
+                        <input type="text" placeholder="Email" />
+                    </div>
+                    <div className="btn">Subscribe</div>
                 </div>
-                <div className="tagline">Unleash Your Inner Warrior</div>
             </div>
-            <div className="footer-right">
-                <div className="footer-links">
-                {
-                    footerLink.map((link, index) => {
-                        return (
-                            <div className="link" key={index}>{link}</div>
-                        )
-                    })
-                }
+            <div className="footer-bottom">
+                <div className="left">
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="tagline">Unleash Your Inner Warrior</div>
                 </div>
-                <div className="sosmed-links">
-                {
-                    footerSosmedIcon.map((icon, index) => {
-                        return (
-                            <div className="link" key={index}>{icon}</div>
-                        )
-                    })
-                }
+                <div className="right">
+                    <div className="links">
+                    {
+                        footerLink.map((link, index) => {
+                            return (
+                                <div className="link" key={index}>{link}</div>
+                            )
+                        })
+                    }
+                    </div>
+                    <div className="sosmed">
+                    {
+                        footerSosmedIcon.map((icon, index) => {
+                            return (
+                                <div className="link" key={index}>{icon}</div>
+                            )
+                        })
+                    }
+                    </div>
+                    <div className="copyright">Privacy Policy | © 2023 Golden Fist</div>
                 </div>
-                <div className="copyright">Privacy Policy | © 2023 Golden Fist</div>
             </div>
         </footer>
     )
